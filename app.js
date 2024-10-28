eb9c3cf869d432cb88aafaccc270feec2acf6e19
Arnav Shekaran <arnav.shekaran@scopious.co.nz>
2024-10-28 14:07:53 +1300

    individual apps

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import ttnWebhooksRouter from "./ttnWebhooks";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-let data = [];
-
 app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.json({
-    data: JSON.stringify(data),
-  });
-});
-
-app.post("/", (req, res) => {
-  data.push(req.body);
-  res.json({
-    message: "Added packet",
-  });
-});
+app.use("/application/:applicationName", ttnWebhooksRouter);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
---
+++ ttnWebhooks.js
@@ -0,0 +1,37 @@
+import express from "express";
+const ttnWebhooksRouter = express.Router();
+
+let data = {};
+
+// middleware that is specific to this router
+const timeLog = (req, res, next) => {
+  console.log(
+    "Time: ",
+    Date.now(),
+    "\tApplication name:",
+    req.params.applicationName
+  );
+  next();
+};
+ttnWebhooksRouter.use(timeLog);
+
+ttnWebhooksRouter.get("/", (req, res) =>
+  res.json({
+    application_name: req.params.applicationName,
+    [`data_for_${req.params.applicationName}`]:
+      data[req.params.applicationName],
+  })
+);
+
+ttnWebhooksRouter.post("/", (req, res) => {
+  if (data[req.params.applicationName]) {
+    data[req.params.applicationName].push(req.body);
+  } else {
+    data[req.params.applicationName] = [req.body];
+  }
+  res.json({
+    message: "Added packet",
+  });
+});
+
+export default ttnWebhooksRouter;
