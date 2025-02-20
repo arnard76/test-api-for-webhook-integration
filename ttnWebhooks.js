fdb5c8b52d88c8fda096ae1670d76b72da290c47
Arnav Shekaran <arnard76@gmail.com>
2025-02-21 12:28:56 +1300

    show request headers for each packet too

--- ttnWebhooks.js
+++ ttnWebhooks.js
@@ -1,7 +1,36 @@
 import express from "express";
+import MILLISECONDS_IN_DAY from "@stdlib/constants-time-milliseconds-in-day";
+import { inspect } from "util";
+import { parse, stringify } from "flatted/esm";
+
 const ttnWebhooksRouter = express.Router({ mergeParams: true });
 
-let data = {};
+const DATA_RETENTION_PERIOD = 14; // days
+const DATA_RETENTION_PERIOD_IN_MS = DATA_RETENTION_PERIOD * MILLISECONDS_IN_DAY;
+
+let packets = {};
+let requests = {};
+let lastClearData = Date.now();
+
+// If in doubt, clear important data from memory 😅😅😅
+function freeUpMemory() {
+  if (Date.now() < lastClearData + DATA_RETENTION_PERIOD_IN_MS) return;
+
+  packets = {};
+  requests = {};
+  lastClearData = Date.now();
+}
+
+/**
+ *
+ * @param {Request<{}, any, any, QueryString.ParsedQs, Record<string, any>>} req
+ */
+function parseImportantRequestDetails(req) {
+  return {
+    headers: req.headers,
+    body: req.body,
+  };
+}
 
 // middleware that is specific to this router
 const timeLog = (req, res, next) => {
@@ -13,29 +42,36 @@ const timeLog = (req, res, next) => {
   );
   next();
 };
+
 ttnWebhooksRouter.use(timeLog);
 
 ttnWebhooksRouter.get("/", (req, res) =>
   res.json({
     application_name: req.params.applicationName,
-    [`data_for_${req.params.applicationName}`]:
-      data[req.params.applicationName],
+    packets: packets[req.params.applicationName],
+    requests: requests[req.params.applicationName],
   })
 );
 
 ttnWebhooksRouter.post("/", (req, res) => {
-  if (data[req.params.applicationName]) {
-    data[req.params.applicationName].push(req.body);
+  freeUpMemory();
+  const parsedReq = parseImportantRequestDetails(req);
+  if (packets[req.params.applicationName]) {
+    packets[req.params.applicationName].push(req.body);
+    requests[req.params.applicationName].push(parsedReq);
   } else {
-    data[req.params.applicationName] = [req.body];
+    packets[req.params.applicationName] = [req.body];
+    requests[req.params.applicationName] = [parsedReq];
   }
+
   res.json({
     message: "Added packet",
   });
 });
 
 ttnWebhooksRouter.get("/clear", (req, res) => {
-  data[req.params.applicationName] = [];
+  packets[req.params.applicationName] = [];
+  requests[req.params.applicationName] = [parseImportantRequestDetails(req)];
   res.json({
     message: "Cleared all packets",
   });
