e44f84e3bdeb0c9fe0da1112237d44b7b5875ef5
Arnav <arnard76@gmail.com>
2025-11-24 08:18:26 +0000

    add cors so that other websites can fetch content from this server (e.g. logs)
    I don't see any danger in this. XSS is the more difficult option to abuse the website content, it would be simpler to just redeploy the website using the public GitHub repo 😂😂

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import ttnWebhooksRouter from "./ttnWebhooks.js";
+import cors from "cors"
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors()); // This allows all origins to access your API.
 app.use(bodyParser.json());
 app.use("/application/:applicationName", ttnWebhooksRouter);
 
